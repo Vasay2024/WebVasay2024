0f1ef2940903c6806741649f0464078033f5a67a
KaVa2024 <157726892+KaVa2024@users.noreply.github.com>
2024-03-17 22:39:09 -0300

    primera subida

---
+++ public/js/script.js
@@ -0,0 +1,18 @@
+// Este script abre la imagen de la miniatura en una nueva pestaña al hacer clic en ella
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Selecciona todas las miniaturas de la galería
+    const thumbnails = document.querySelectorAll('.art-thumbnail');
+
+    // Asigna un escuchador de eventos a cada miniatura
+    thumbnails.forEach(thumbnail => {
+        thumbnail.addEventListener('click', function(event) {
+            // Previene el comportamiento predeterminado del navegador
+            event.preventDefault();
+            // Obtiene la URL de la imagen de la miniatura
+            const imageUrl = thumbnail.getAttribute('src');
+            // Abre la imagen en una nueva pestaña del navegador
+            window.open(imageUrl, '_blank');
+        });
+    });
+});
