017b1b0f89278c97885cad7effd64353089ff64d
Vasay2024 <vasay2.2024@gmail.com>
2024-03-19 19:37:49 -0300

    sitio web responsivo

--- public/js/script.js
+++ public/js/script.js
@@ -1,18 +1,10 @@
-/* Este script abre la imagen de la miniatura en una nueva pestaña al hacer clic en ella
-
 document.addEventListener('DOMContentLoaded', function() {
-    // Selecciona todas las miniaturas de la galería
-    const thumbnails = document.querySelectorAll('.art-thumbnail');
+    var menuToggle = document.querySelector('.menu-toggle');
+    var primaryMenu = document.getElementById('primary-menu');
 
-    // Asigna un escuchador de eventos a cada miniatura
-    thumbnails.forEach(thumbnail => {
-        thumbnail.addEventListener('click', function(event) {
-            // Previene el comportamiento predeterminado del navegador
-            event.preventDefault();
-            // Obtiene la URL de la imagen de la miniatura
-            const imageUrl = thumbnail.getAttribute('src');
-            // Abre la imagen en una nueva pestaña del navegador
-            window.open(imageUrl, '_blank');
-        });
+    menuToggle.addEventListener('click', function() {
+        var isExpanded = menuToggle.getAttribute('aria-expanded') === 'true';
+        menuToggle.setAttribute('aria-expanded', !isExpanded);
+        primaryMenu.style.display = isExpanded ? 'none' : 'block';
     });
-});*/
+});
